96c4247741ecbc27436a565c952d04713ee81a7e
SoloTufi <mat_990011@mail.ru>
2024-07-10 22:29:09 +0700

    Garbage disposal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css'
+import React from 'react'
 
-function App() {
-	return (
-		<>
-			<div className=''></div>
-		</>
-	)
+const App = () => {
+	return <div>App</div>
 }
 
 export default App
