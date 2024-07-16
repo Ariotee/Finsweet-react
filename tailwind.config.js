ce8a76ab77150eb65ca74bb5136ef35b85182340
SoloTufi <mat_990011@mail.ru>
2024-07-17 01:44:03 +0700

    add/new-themes-for-tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './src/components/**/*.{js,jsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#232536',
+      },
+    },
+    fontFamily: {
+      sen: ['Sen', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+    },
+  },
+  plugins: [],
 }
