d694cd5e72fa9711ef53332279659734357303d9
SoloTufi <mat_990011@mail.ru>
2024-07-17 01:41:49 +0700

    edited-imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '~/app/App'
+
+import App from '~/App'
+import './scss/all.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
