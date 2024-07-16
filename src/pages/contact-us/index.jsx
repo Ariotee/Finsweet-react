c14e6cf30a3218127c015dc9f163820e88ffee5e
SoloTufi <mat_990011@mail.ru>
2024-07-17 03:13:59 +0700

    created/contact-us

---
+++ src/pages/contact-us/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from '../../shared/shells'
+
+const ContactUsPage = () => {
+  return (
+    <div className=''>
+      <Container>Contact Us</Container>
+    </div>
+  )
+}
+
+export default ContactUsPage
