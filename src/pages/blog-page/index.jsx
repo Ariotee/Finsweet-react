2ea7810ac62aa0195b3a41ec1e9b8bf31a8fbc63
SoloTufi <mat_990011@mail.ru>
2024-07-17 03:14:17 +0700

    created/blog-page

---
+++ src/pages/blog-page/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from '../../shared/shells'
+
+const BlogPage = () => {
+  return (
+    <section className=''>
+      <Container>BlogPage</Container>
+    </section>
+  )
+}
+
+export default BlogPage
