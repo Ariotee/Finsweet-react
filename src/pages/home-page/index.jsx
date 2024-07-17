888229ed8a48f6f802e058678e1c63427de3cc82
SoloTufi <mat_990011@mail.ru>
2024-07-18 02:51:20 +0700

    connect/add-post-section

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import Introduction from '~/components/sections/introduction'
+import Posts from '../../components/sections/posts'
 
 const HomePage = () => {
-  return <Introduction />
+  return (
+    <>
+      <Introduction />
+      <Posts />
+    </>
+  )
 }
 
 export default HomePage
