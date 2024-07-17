eb190ee64c6dafee9ff7154542c7dd89a9e708cd
SoloTufi <mat_990011@mail.ru>
2024-07-18 02:50:41 +0700

    added/new-tw-themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,18 @@ export default {
       colors: {
         black: '#232536',
         accent: '#ffd050',
+        'accent-second': '#592ea9',
         'accent-dark': '#cda73f',
+        'border-dark': 'rgba(109, 110, 118, 0.1)',
+        'light-dark': '#4c4c4c',
+        gray: '#6d6e76',
+        purple: '#592ea9',
       },
       backgroundImage: {
         'intro-bg': 'url(../../src/assets/img/intro-bg.png)',
+      },
+      spacing: {
+        pcard: '25px 116px 25px 32px',
       },
     },
     fontFamily: {
