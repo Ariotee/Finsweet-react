6c728a1d4d30ab21f0463dcab5b599865d1b43d6
SoloTufi <mat_990011@mail.ru>
2024-07-21 02:40:39 +0700

    create/ui-for-about-us

---
+++ src/components/sections/about-us/ui/TextFields.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const TextFieldSubTitle = ({ children }) => {
+  return (
+    <p className='font-inter font-semibold text-base leading-[125%] tracking-[0.19em] uppercase text-black'>
+      {children}
+    </p>
+  )
+}
+
+export const TextFieldTitle = ({ children }) => {
+  return (
+    <h2 className='font-sen mt-6 mb-4 font-bold text-4xl leading-[133%] tracking-[-0.06em] text-black'>
+      {children}
+    </h2>
+  )
+}
+
+export const TextFieldInfo = ({ children }) => {
+  return (
+    <p className='font-inter font-normal text-base leading-[175%] text-gray'>
+      {children}
+    </p>
+  )
+}
