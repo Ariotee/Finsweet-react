84223726062dfc1cfa44481ba4502d77e731a6e8
SoloTufi <mat_990011@mail.ru>
2024-07-21 20:53:57 +0700

    create/partners

---
+++ src/components/sections/partners/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { PartnersContent } from './data/partners.data'
+
+import { Container } from '~/shared/shells'
+import { PartnerItem } from './ui/PartnerItem'
+
+const Partners = () => {
+  return (
+    <div className='pb-5'>
+      <Container>
+        <ul className='flex items-center justify-between'>
+          {PartnersContent?.map(obj => (
+            <PartnerItem key={obj.id} obj={obj} />
+          ))}
+        </ul>
+      </Container>
+    </div>
+  )
+}
+
+export default Partners
