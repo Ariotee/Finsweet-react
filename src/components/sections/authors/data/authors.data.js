cc83855f1d5712440997407c88cf959e57f58b60
SoloTufi <mat_990011@mail.ru>
2024-07-21 20:32:55 +0700

    init/data-for-authors

---
+++ src/components/sections/authors/data/authors.data.js
@@ -0,0 +1,52 @@
+import FloydMiles from '~/assets/img/floyd-miles.png'
+import DianneRussell from '~/assets/img/dianne-russell.png'
+import JennyWilson from '~/assets/img/jenny-wilson.png'
+import LeslieAlexander from '~/assets/img/leslie-alexander.png'
+
+import TwiterIcon from '~/assets/svg/twitter.svg'
+import FacebookIcon from '~/assets/svg/facebook.svg'
+import InstagramIcon from '~/assets/svg/instagram.svg'
+import LinkedinIcon from '~/assets/svg/linkedin.svg'
+
+export const AuthorsContent = [
+  {
+    id: 0,
+    avatarUrl: FloydMiles,
+    name: 'Floyd Miles',
+    companyName: 'Google',
+    facebookIconUrl: FacebookIcon,
+    twiterIconUrl: TwiterIcon,
+    instagramIconUrl: InstagramIcon,
+    linkedinIconUrl: LinkedinIcon,
+  },
+  {
+    id: 1,
+    avatarUrl: DianneRussell,
+    name: 'Dianne Russell',
+    companyName: 'Facebook',
+    facebookIconUrl: FacebookIcon,
+    twiterIconUrl: TwiterIcon,
+    instagramIconUrl: InstagramIcon,
+    linkedinIconUrl: LinkedinIcon,
+  },
+  {
+    id: 2,
+    avatarUrl: JennyWilson,
+    name: 'Jenny Wilson',
+    companyName: 'Amazon',
+    facebookIconUrl: FacebookIcon,
+    twiterIconUrl: TwiterIcon,
+    instagramIconUrl: InstagramIcon,
+    linkedinIconUrl: LinkedinIcon,
+  },
+  {
+    id: 3,
+    avatarUrl: LeslieAlexander,
+    name: 'Leslie Alexander',
+    companyName: 'PoPivu',
+    facebookIconUrl: FacebookIcon,
+    twiterIconUrl: TwiterIcon,
+    instagramIconUrl: InstagramIcon,
+    linkedinIconUrl: LinkedinIcon,
+  },
+]
