67bf9e5959549c5d13bb2cc91e8a1312a19412f2
SoloTufi <mat_990011@mail.ru>
2024-07-22 03:55:53 +0700

    create/reviews-slide-component

---
+++ src/components/sections/reviews/ui/Slide.jsx
@@ -0,0 +1,25 @@
+import SwiperAvatar from '~/assets/img/avatarslide.png'
+
+export const Slide = ({ children }) => {
+  return (
+    <div className='flex flex-col gap-[120px] max-w-[530px]'>
+      <p className='font-sen font-bold text-2xl leading-[133%] text-black'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+
+      <div className='flex items-center gap-4'>
+        <img src={SwiperAvatar} />
+        <div className=''>
+          <p className=' font-sen font-bold text-2xl leading-[133%] text-black'>
+            Jonathan Vallem
+          </p>
+          <span className='font-inter font-normal text-base leading-[175%] text-gray'>
+            New york, USA
+          </span>
+        </div>
+        {children}
+      </div>
+    </div>
+  )
+}
