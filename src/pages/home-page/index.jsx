f9932fd88d02e7f77ba7de3a7109233211ca5c65
SoloTufi <mat_990011@mail.ru>
2024-07-22 22:35:15 +0700

    refactor/edit/for(4fbe816)

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Introduction from '~/components/sections/introduction'
 import Posts from '~/components/sections/posts'
 import AboutUs from '~/components/sections/about-us'
