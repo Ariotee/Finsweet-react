f0d5f5efec0b357021a74f050d7fb3c54fb3bd36
SoloTufi <mat_990011@mail.ru>
2024-07-22 22:34:31 +0700

    refactor/edit/for(4fbe816)

--- src/pages/layouts/MainLayout.jsx
+++ src/pages/layouts/MainLayout.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 import { Outlet } from 'react-router-dom'
 
-import Header from '~/components/header/ui'
+import Header from '~/components/header/'
 import Footer from '~/components/footer'
 
 const MainLayout = () => {
