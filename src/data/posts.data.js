51fb2d1a865f0c9ab592a2bee9481de0e9f37242
SoloTufi <mat_990011@mail.ru>
2024-07-29 23:53:40 +0700

    init/data-posts

---
+++ src/data/posts.data.js
@@ -0,0 +1,42 @@
+import postIll1 from '~/assets/img/posts/post-ill-1.png'
+import postIll2 from '~/assets/img/posts/post-ill-2.png'
+import postIll3 from '~/assets/img/posts/post-ill-3.png'
+import postIll4 from '~/assets/img/posts/post-ill-4.png'
+
+export const allposts = [
+  {
+    id: 0,
+    imageURL: postIll1,
+    subtitle: 'Startup',
+    title: 'Design tips for designers that cover everything you need',
+    info: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+  },
+  {
+    id: 1,
+    imageURL: postIll2,
+    subtitle: 'Business',
+    title: 'How to build rapport with your web design clients',
+    info: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+  },
+  {
+    id: 2,
+    imageURL: postIll3,
+    subtitle: 'Startup',
+    title: 'Logo design trends to avoid in 2022',
+    info: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+  },
+  {
+    id: 3,
+    imageURL: postIll4,
+    subtitle: 'Technology',
+    title: '8 Figma design systems you can download for free today',
+    info: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+  },
+  {
+    id: 4,
+    imageURL: postIll2,
+    subtitle: 'Economy',
+    title: 'Font sizes in UI design: The complete guide to follow',
+    info: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+  },
+]
